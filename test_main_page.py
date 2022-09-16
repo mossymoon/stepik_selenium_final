e5db8da3f37bac2499e2cf2bcd5195a46fa9371b
mossymoon <stephan.sergienko@gmail.com>
2022-09-16 10:47:20 +0300

    add_page_object_structure2

--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,7 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    page.should_be_login_link()  # проверка наличия ссылки на логин
 
 def go_to_login_page(self, browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
