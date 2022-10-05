107ef3f9ae656953375282752aa544419ed4c1a1
mossymoon <stephan.sergienko@gmail.com>
2022-10-05 21:17:32 +0400

    login_page_assertions_update

--- test_main_page.py
+++ test_main_page.py
@@ -4,16 +4,12 @@ from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    login = LoginPage(browser, url)
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # переход на страницу логина
-    page.should_be_login_link()  # проверка наличия ссылки на логин
-    login.should_be_login_url()  # проверка наличия правильного url
-    login.should_be_login_form()  # проверка наличия формы авторизации
-    login.should_be_register_form()  # проверка наличия формы регистрации
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def go_to_login_page(self, browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
